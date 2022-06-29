8e98ff110f25cc3514e2e6cf072b1b1de5992d95
prashant3jan <prashant3jan@gmail.com>
2022-06-29 09:56:09 +0530

    committing ActionbarVideo project

---
+++ ActionbarVideo/app/src/main/java/com/jurrius/actionbarvideo/MainActivity2.java
@@ -0,0 +1,22 @@
+package com.jurrius.actionbarvideo;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setIcon(R.mipmap.chome);
+        actionBar.setTitle(" Second Activity");
+        actionBar.setDisplayUseLogoEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+}
